e4ee59188c29fe8f3e3c3001c760a717fd254e93
Crowdbotics <team@crowdbotics.com>
2021-03-18 19:04:53 +0000

    Update models

--- backend/spy_time_25106/wsgi.py
+++ backend/spy_time_25106/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spy_time_25106.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spy_time_25106.settings")
 
 application = get_wsgi_application()
